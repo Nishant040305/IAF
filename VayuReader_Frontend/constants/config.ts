f9b3aa0cf04961dbd0e0df2ced78946d09be9089
aachintya <aachinty11@gmail.com>
2026-01-11 10:27:26 +0530

    fixed conflict bugs

--- VayuReader_Frontend/constants/config.ts
+++ VayuReader_Frontend/constants/config.ts
@@ -1,6 +1,6 @@
 const env = (globalThis as any)?.process?.env || {};
-const backendUrl = env.EXPO_PUBLIC_BACKEND_URL || 'https://iaf-00xf.onrender.com';
-export const AUTH_BASE_URL = backendUrl;
-export const PDF_BASE_URL = backendUrl;
-export const DICT_BASE_URL = backendUrl;
-export const ABBR_BASE_URL = backendUrl;
+const backendUrl = env.EXPO_PUBLIC_AUTH_BASE_URL || env.EXPO_PUBLIC_BACKEND_URL || 'http://localhost:3000';
+export const AUTH_BASE_URL = env.EXPO_PUBLIC_AUTH_BASE_URL || backendUrl;
+export const PDF_BASE_URL = env.EXPO_PUBLIC_PDF_BASE_URL || backendUrl;
+export const DICT_BASE_URL = env.EXPO_PUBLIC_DICT_BASE_URL || backendUrl;
+export const ABBR_BASE_URL = env.EXPO_PUBLIC_ABBR_BASE_URL || backendUrl;
